73bf5bbdaeb1bac3097fa4ef64e10e10d3609f5e
Luiz Antonio Motta <mottaluizantonio@gmail.com>
2022-09-01 21:29:24 -0300

    feat: header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+import Search from '../assets/search.svg';
+import Account from '../assets/account.svg';
+import Winebox from '../assets/winebox.svg';
+import Winelogo from '../assets/wineLogo.svg';
+import {
+  Container,
+  ContainerLeft,
+  ContainerRight
+} from '../styles/components/Header';
+
+const Header = () => (
+  <Container>
+    <ContainerLeft>
+      <Winelogo />
+      <ul>
+        <li>Clube</li>
+        <li>Loja</li>
+        <li>Produtores</li>
+        <li>Ofertas</li>
+        <li>Eventos</li>
+      </ul>
+    </ContainerLeft>
+    <ContainerRight>
+      <Search />
+      <Account />
+      <Winebox />
+    </ContainerRight>
+  </Container>
+);
+
+export default Header;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,10 @@
 const theme = {
   colors: {
-    background: '#e1e1e5',
+    grayDefault: '#555555',
+    white: '#FFFFFF',
+    lightTannat: '#D14B8F',
+    graySilver: '#CCCCCC',
+    background: '#F5F5F5',
     text: '#121214',
     primary: '#8257e6'
   }
