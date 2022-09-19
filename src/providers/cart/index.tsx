cfea1506e080e2687887328815ef7621a31acbcc
Luiz Antonio Motta <mottaluizantonio@gmail.com>
2022-09-19 14:12:54 -0300

    fix: functions addToCart and removeFromCart now have proper types. Cart page now have a proper description

--- src/providers/cart/index.tsx
+++ src/providers/cart/index.tsx
@@ -25,7 +25,7 @@ export const CartProvider = ({ children }) => {
     localStorage.setItem('cartwinelam', JSON.stringify(cart));
   }, [cart]);
 
-  const addToCart = (product, productQuantity) => {
+  const addToCart = (product: Item, productQuantity: number) => {
     const productExist = cart.find(({ id }) => id === product.id);
 
     if (!productExist) {
@@ -40,7 +40,7 @@ export const CartProvider = ({ children }) => {
     }
   };
 
-  const removeFromCart = (product, remove) => {
+  const removeFromCart = (product: ItemCart, remove?: boolean) => {
     if (remove || product.quantity === 1) {
       const newCartList = cart.filter(({ id }) => id !== product.id);
       setCart(newCartList);
